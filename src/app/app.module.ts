2316ec9a0e31ab123916c2b319519ac78d74da20
Christian Palazzo <info@christianpalazzo.org>
2017-05-17 17:57:06 +0200

    slide menu implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,36 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { MenuComponent } from './menu/menu.component';
+import { SlideMenuComponent } from './slide-menu/slide-menu.component';
 
 /**
  * routing
  */
 const appRoutes: Routes = [
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'contacts', component: ContactsComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ContactsComponent,
+    MenuComponent,
+    SlideMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
